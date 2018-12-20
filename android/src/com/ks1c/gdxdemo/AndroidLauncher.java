204d5ebc8737cbc6b9a60c18baa480397606f5ce
ks1c <ksic3784@gmail.com>
2018-12-19 21:07:41 -0300

    Initial commit

---
+++ android/src/com/ks1c/gdxdemo/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ks1c.gdxdemo;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ks1c.gdxdemo.GdxDemo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GdxDemo(), config);
+	}
+}
---
+++ desktop/src/com/ks1c/gdxdemo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ks1c.gdxdemo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ks1c.gdxdemo.GdxDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxDemo(), config);
+	}
+}
