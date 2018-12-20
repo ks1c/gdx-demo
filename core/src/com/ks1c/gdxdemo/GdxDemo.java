7349bb11847ed95409e1d90830f949e5bf57b931
ks1c <ksic3784@gmail.com>
2018-12-19 21:43:12 -0300

    GAME_WIDTH GAME_HEIGHT

--- core/src/com/ks1c/gdxdemo/GdxDemo.java
+++ core/src/com/ks1c/gdxdemo/GdxDemo.java
@@ -1,33 +1,16 @@
 package com.ks1c.gdxdemo;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.ScreenAdapter;
 
-public class GdxDemo extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class GdxDemo extends Game {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    public static final int GAME_HEIGHT = 540;
+    public static final int GAME_WIDTH = 960;
+
+    @Override
+    public void create() {
+
+        setScreen(new ScreenAdapter());
+    }
 }
