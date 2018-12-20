ceb0bd27f7225eb8731db014d20be0f9615bfa25
ks1c <ksic3784@gmail.com>
2018-12-19 21:19:49 -0300

    desktop update

--- desktop/src/com/ks1c/gdxdemo/desktop/DesktopLauncher.java
+++ desktop/src/com/ks1c/gdxdemo/desktop/DesktopLauncher.java
@@ -5,8 +5,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ks1c.gdxdemo.GdxDemo;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new GdxDemo(), config);
-	}
+    public static void main(String[] arg) {
+
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+        config.title = "Prototype";
+        config.width = 960;
+        config.height = 540;
+
+        new LwjglApplication(new GdxDemo(), config);
+    }
 }
